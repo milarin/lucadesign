f3a19fad3e1ff5e710deb6da11f3518f92cf69d6
milan <acmilanpooh@gmail.com>
2021-05-25 15:53:44 +0900

    first

---
+++ 案件/コワーキング/table-about/sample.js
@@ -0,0 +1,14 @@
+$(window).on('load',function(){
+  // ここから文字を<span></span>で囲む記述
+  $('.typ').children().andSelf().contents().each(function() {
+  if (this.nodeType == 3) {
+  $(this).replaceWith($(this).text().replace(/(\S)/g, '<span>$1</span>'));
+  }
+  });
+  // ここから一文字ずつフェードインさせる記述
+  $('.typ').css({'opacity':1});
+  for (var i = 0; i <= $('.typ').children().size(); i++) {
+  $('.typ').children('span:eq('+i+')').delay(50*i).animate({'opacity':1},50);
+  };
+  });
+
